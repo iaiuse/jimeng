e750be4da598969fcdf102b37fc1bf438dd6dfc8
richardson <chenyb@uyunsoft.cn>
2025-04-06 20:43:37 +0800

    完成材质部分的内容

--- src/data/response-dictionary/material.ts
+++ src/data/response-dictionary/material.ts
@@ -1,9 +1,56 @@
 export const materialItems = [
-    { 
-      name: '金属', 
-      description: '金属材质的表现效果',
-      filename: 'material/metal.webp',
-      alt: '金属材质'
-    },
-    // ... 其他材质条目 ...
-  ]
+  {
+      name: '毛绒',
+      description: '毛绒材质的表现效果',
+      filename: 'material/image001.webp',
+      alt: '毛绒材质苹果'
+  },
+  {
+      name: '毛毡',
+      description: '毛毡材质的表现效果',
+      filename: 'material/image002.webp',
+      alt: '毛毡材质梨子'
+  },
+  {
+      name: '当代玻璃艺术',
+      description: '现代玻璃艺术的表现效果',
+      filename: 'material/image003.webp',
+      alt: '玻璃艺术苹果'
+  },
+  {
+      name: '青铜器质感',
+      description: '青铜器材质的表现效果',
+      filename: 'material/image004.webp',
+      alt: '青铜器苹果'
+  },
+  {
+      name: '粘土材质',
+      description: '粘土材质的表现效果',
+      filename: 'material/image005.webp',
+      alt: '粘土猫咪'
+  },
+  {
+      name: '彩色玻璃材质',
+      description: '彩色玻璃材质的表现效果',
+      filename: 'material/image006.webp',
+      alt: '彩色玻璃猫咪'
+  },
+  {
+      name: '果冻材质',
+      description: '果冻材质的表现效果',
+      filename: 'material/image007.webp',
+      alt: '果冻材质猫咪'
+  },
+  {
+      name: '烫金材质',
+      description: '烫金材质的表现效果',
+      filename: 'material/image008.webp',
+      alt: '烫金材质猫咪'
+  },
+  {
+      name: '油漆质感',
+      description: '油漆材质的表现效果',
+      filename: 'material/image009.webp',
+      alt: '油漆材质猫咪'
+  }
+]
